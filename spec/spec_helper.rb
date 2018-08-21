aec6c290443aa3a361a964ad03819c3bfcf278db
arunpatyal <Sumologicarun1983>
2018-08-21 18:09:32 +0530

    Build config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,34 +3,34 @@ require 'rspec-puppet-facts'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
-begin
-  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
-rescue LoadError => loaderror
-  warn "Could not require spec_helper_local: #{loaderror.message}"
-end
+#begin
+#  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+#rescue LoadError => loaderror
+#  warn "Could not require spec_helper_local: #{loaderror.message}"
+#end
 
-include RspecPuppetFacts
-default_facts = {
-  puppetversion: Puppet.version,
-  facterversion: Facter.version,
-}
+#include RspecPuppetFacts
+#default_facts = {
+#  puppetversion: Puppet.version,
+#  facterversion: Facter.version,
+#}
 
-default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
-default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+#default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+#default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
 
-if File.exist?(default_facts_path) && File.readable?(default_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
-end
+#if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+#  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+#end
 
-if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
-  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
-end
+#if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+#  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+#end
 
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.environmentpath = File.join(Dir.pwd, 'spec')
-  c.default_facts = default_facts
+#  c.default_facts = default_facts
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
